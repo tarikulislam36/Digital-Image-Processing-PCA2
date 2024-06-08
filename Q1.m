77f89a655eba73c859ddd50701b9dc3dd46142b0
tarikulislam <tarikulislam4145@gmail.com>
2024-06-08 23:09:04 +0530

    Q1

---
+++ Q1.m
@@ -0,0 +1,10 @@
+img = imread('dog.jpg');
+		grayIMG = im2gray(img);
+		levels = [128, 64, 32, 16, 8];
+		figure;
+		for i = 1:length(levels)
+			subplot(1,5,i);
+			imshow(uint8(floor(double(grayIMG) / 256 * levels(i)) * (256 / levels(i))));
+			title([num2str(levels(i)) ' gray levels']);
+        
+        end
